777c6d193d976a8a55512e864bf11d79fbd383a2
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2019-01-25 21:56:01 +0100

    fixed some things

--- source/engine/managers/render_manager.hpp
+++ source/engine/managers/render_manager.hpp
@@ -1,26 +1,15 @@
 #pragma once
+
 #include "../engine_api.hpp"
-#include "GLM/glm.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
 
 class SpriteComponent;
 
 class ENGINE_API RenderManager 
 {
-    /// Render world containing entities.
-    /**
-     * @param world Contains a bunch of entities.
-     * @param targetDisplay Display type to render.
-     * @param soundSources Whether to show sound sources.
-     * @param particleEmitters Whether to show particle emitters.
-     * @param lightSources Whether to show light sources.
-     * @param cameras Whether to show cameras.
-     * @param physics Whether to show physics volumes.
-     * @param camera Camera through which to render (or first camera in the world if nullptr).
-     * @param lighting Whether to light the scene (otherwise full ambient is used).
-     * @param lightVolumes Whether to show light culling volumes.
-     */
-    void Render();
+public:
+
+    void render();
 
 private:
     RenderManager();
