28f6efaeb5cd2e8ea818249888a6e026cfd1d3c1
travisko <marek.cern@gmail.com>
2019-01-27 11:39:04 +0100

    Adding AI new movement strategy.

---
+++ source/game/left_circling_strategy.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "i_movement_strategy.hpp"
+#include <SFML/Graphics.hpp>
+
+class LeftCirclingStrategy : public IMovementStrategy
+{
+public:
+    // just leave the direction as it is without any movement speed
+    void update_movement(Enemy::Direction &move_dir, int &target_x, int &target_y) override;
+};
